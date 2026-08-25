e22bb098a7b7900da66dd09fdd864699e2c5f4d6
Fatihx45 <fatih.olmez@kun.edu.tr>
2026-08-25 04:27:33 +0300

    refactor: Remove redundant Shop and Forge buttons from main menu body as they already exist on top app bar

--- derin-kazi_app/test/widget_test.dart
+++ derin-kazi_app/test/widget_test.dart
@@ -17,8 +17,8 @@ void main() {
     // Ana Menü Elemanları
     expect(find.byType(Image), findsWidgets);
     expect(find.textContaining('OYUNA BAŞLA'), findsOneWidget);
-    expect(find.textContaining('SİLAH & MERMİ MAĞAZASI'), findsOneWidget);
-    expect(find.textContaining('ATÖLYE & GÜÇLENDİRME'), findsOneWidget);
+    expect(find.textContaining('EKİP KAZISI'), findsOneWidget);
+    expect(find.textContaining('GÖREVLER & ÖDÜLLER'), findsOneWidget);
 
     // Oyuna Başla'ya tıkla (Bölüm Seçim Ekranı Açılır)
     await tester.tap(find.textContaining('OYUNA BAŞLA'));
@@ -40,11 +40,11 @@ void main() {
     await tester.tap(find.text('MAĞAZA'));
     await tester.pumpAndSettle();
 
-    // Dialog açıldı mı?
+    // Mağaza dialogu açık mı?
     expect(find.text('MAĞAZA & YÜKSELTMELER'), findsOneWidget);
 
-    // Temizlik
+    // Temizle
+    await tester.pumpWidget(const SizedBox());
     container.dispose();
   });
 }
-
